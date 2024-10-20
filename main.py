b80d3729bffc04a822301635df5cf8bc1f09ae02
skoumann <frbohe@gmail.com>
2024-10-20 09:39:22 +0200

    Added clock ticker

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import*
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -11,6 +13,8 @@ def main():
 
     while pygame.get_init():
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick() / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
